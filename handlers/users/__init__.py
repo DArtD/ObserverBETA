ea69b9f355b904e5d2f343c86596bef33aee3547
DArtD <114067414+DArtD@users.noreply.github.com>
2023-04-27 19:25:37 +0500

    добавлена комада Links добавлена полая клавиатура choice

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -5,7 +5,7 @@ from .timetable_next import dp
 from .timetable_edit import dp
 from .get_list import dp
 from .get_list_tx import dp
-from .Links import dp
+from .links import dp
 from .start import dp
 from .doc import dp
 from .get_weather import dp
