480bc0903fa8a9e66215cd5eb0a5e5b1fff5b835
DArtD <114067414+DArtD@users.noreply.github.com>
2023-04-27 19:44:56 +0500

    заменен токен бота

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 group_chat = "-1001844038287"
-BOT_TOKEN = '6008950844:AAEzzRDQO12uDLN74hfMbSZDNE6qRBJ2Lbc'
+BOT_TOKEN = '6008950844:AAGZx5poYNOPH1EX-M50pBUsgmmQzqqbCMg'
 admin = '1982872796'
 OPEN_WEATHER_TOKEN = "d7d94cd3ac0212c52fc2f6af7569d737"
 
