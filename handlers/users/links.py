998d36a84dcbcecf442ab8df503ecc58415bbd5b
DArtD <114067414+DArtD@users.noreply.github.com>
2023-04-27 20:09:07 +0500

    links IsPrivate()

--- handlers/users/links.py
+++ handlers/users/links.py
@@ -8,4 +8,4 @@ from loader import dp
 
 @dp.message_handler(Command("Links"), IsPrivate())
 async def links(message: types.Message):
-    await message.answer(text="Полезные ссылки", reply_markup=choice)
+    await message.answer(text="     Полезные ссылки", reply_markup=choice)
