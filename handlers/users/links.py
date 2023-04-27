8fdb2af05ef52294ca473de5b046c9bbd49068f8
DArtD <114067414+DArtD@users.noreply.github.com>
2023-04-27 20:01:54 +0500

    links IsPrivate()

--- handlers/users/links.py
+++ handlers/users/links.py
@@ -1,10 +1,11 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Command
 
+from filters.IsPrivate import IsPrivate
 from keyboards.inline.choice import choice
 from loader import dp
 
 
-@dp.message_handler(Command("Links"))
+@dp.message_handler(Command("Links"), IsPrivate())
 async def links(message: types.Message):
     await message.answer(text="Полезные ссылки", reply_markup=choice)
