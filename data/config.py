413b6de5631376a08a2e33789fdde13d91cad4ea
DArtD <114067414+DArtD@users.noreply.github.com>
2023-04-28 20:17:51 +0500

    исправлена задача для расписания

--- data/config.py
+++ data/config.py
@@ -8,6 +8,7 @@ group_chat = "-1001844038287"
 BOT_TOKEN = '6008950844:AAGZx5poYNOPH1EX-M50pBUsgmmQzqqbCMg'
 admin = '1982872796'
 OPEN_WEATHER_TOKEN = "d7d94cd3ac0212c52fc2f6af7569d737"
+GPT_KEY = "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"
 
 ip = os.getenv("ip")
 
--- utils/timetable_next_cron.py
+++ utils/timetable_next_cron.py
@@ -6,24 +6,35 @@ from utils.Upper_Lower_week import UpperLowerweek
 
 
 async def timetable_next(bot: Bot):
+
     dt_day = datetime.datetime.today() + datetime.timedelta(hours=5)
-    if 4 <= dt_day.weekday() <= 6:
-        dt = 0
-        check = True
-    else:
-        dt = dt_day.weekday() + 1
-        check = False
 
-    timetable = ''
-    with open('timetable.json', encoding="utf-8") as f:
-        obj1 = f.read()
-        obj = json.loads(obj1)
-        if dt == 3 and await UpperLowerweek(nx_week=check) == "<b>Нижняя неделя</b>":
-            timetable += '\n' + obj["3"]["first_pair"] + '\n' + obj["3"]["second_pair"]
+    if 0 <= dt_day.weekday() <= 3 or dt_day.weekday() == 6:
+
+        if dt_day.weekday() == 6:
+            dt = 0
+            check = True
+
         else:
-            for i in obj[str(dt)]:
-                timetable += '\n' + obj[str(dt)][i] + '\n'
+            dt = dt_day.weekday() + 1
+            check = False
 
-        return await bot.send_message(chat_id=group_chat, text="<b><u>Расписание на завтра</u></b>"),\
-               await bot.send_message(chat_id=group_chat, text=await UpperLowerweek(nx_week=check), disable_notification=True), \
-               await bot.send_message(chat_id=group_chat, text=timetable, disable_notification=True)
+        timetable = ''
+
+        with open('timetable.json', encoding="utf-8") as f:
+            obj1 = f.read()
+            obj = json.loads(obj1)
+
+            if dt == 3 and await UpperLowerweek(nx_week=check) == "<b>Нижняя неделя</b>":
+                timetable += '\n' + obj["3"]["first_pair"] + '\n' + obj["3"]["second_pair"]
+
+            else:
+                for i in obj[str(dt)]:
+                    timetable += '\n' + obj[str(dt)][i] + '\n'
+
+            return await bot.send_message(chat_id=group_chat, text="<b><u>Расписание на завтра</u></b>"),\
+                   await bot.send_message(chat_id=group_chat, text=await UpperLowerweek(nx_week=check), disable_notification=True), \
+                   await bot.send_message(chat_id=group_chat, text=timetable, disable_notification=True)
+
+    else:
+        return
