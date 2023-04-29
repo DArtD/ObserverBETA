a69ed3406b626ea3a963a8e78a46a7b632f1e0ec
DArtD <114067414+DArtD@users.noreply.github.com>
2023-04-29 20:31:23 +0500

    исправлена ошибка лишнего выкладывания расписания

--- utils/timetable_today_cron.py
+++ utils/timetable_today_cron.py
@@ -10,30 +10,24 @@ async def timetable_today_cron(bot: Bot):
 
     dt_day = datetime.datetime.today() + datetime.timedelta(hours=5)
     dt = dt_day.weekday()
-    if 0 <= dt <= 5:
 
-        if 0 <= dt <= 4:
-            timetable = ''
+    if 0 <= dt <= 4:
+        timetable = ''
 
-            with open('timetable.json', encoding="utf-8") as f:
-                obj1 = f.read()
-                obj = json.loads(obj1)
+        with open('timetable.json', encoding="utf-8") as f:
+            obj1 = f.read()
+            obj = json.loads(obj1)
 
-                if dt == 3 and await UpperLowerweek() == "<b>Нижняя неделя</b>":
-                    timetable += '\n' + obj["3"]["first_pair"] + '\n' + obj["3"]["second_pair"]
+            if dt == 3 and await UpperLowerweek() == "<b>Нижняя неделя</b>":
+                timetable += '\n' + obj["3"]["first_pair"] + '\n' + obj["3"]["second_pair"]
 
-                else:
-                    for i in obj[str(dt)]:
-                        timetable += '\n' + obj[str(dt)][i] + '\n'
+            else:
+                for i in obj[str(dt)]:
+                    timetable += '\n' + obj[str(dt)][i] + '\n'
 
-                return await bot.send_message(chat_id=group_chat, text=await get_weather(), disable_notification=True), \
-                       await bot.send_message(chat_id=group_chat, text=await UpperLowerweek(), disable_notification=True), \
-                       await bot.send_message(chat_id=group_chat, text=timetable, disable_notification=True)
-
-        else:
             return await bot.send_message(chat_id=group_chat, text=await get_weather(), disable_notification=True), \
                    await bot.send_message(chat_id=group_chat, text=await UpperLowerweek(), disable_notification=True), \
-                   await bot.send_message(chat_id=group_chat, text="<b>Сегодня нет пар</b>", disable_notification=True)
+                   await bot.send_message(chat_id=group_chat, text=timetable, disable_notification=True)
 
     else:
         return
