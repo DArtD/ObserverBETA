62cddd881b17f89fdb1bd756f0f8ee83861568bb
DArtD <114067414+DArtD@users.noreply.github.com>
2023-05-01 17:56:17 +0500

    добавлена функция forward_messages

--- utils/forward_messages.py
+++ utils/forward_messages.py
@@ -4,6 +4,6 @@ from aiogram import types
 
 async def forward_messages(message: types.Message):
     if message.chat.type == types.ChatType.PRIVATE:
-        return await dp.bot.send_message(chat_id=-859544611, text=f"{message.from_user.full_name, message.from_user.id}"
-                                                                  f"\n'{message.text}'")
-
+        return await dp.bot.forward_message(chat_id=-859544611,
+                                            from_chat_id=message.chat.id,
+                                            message_id=message.message_id)
